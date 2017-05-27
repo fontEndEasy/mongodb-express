a94c3fba41202d864f17309a1512fcce421914f9
^+xl.z)` <fontEndEasy@users.noreply.github.com>
2017-05-27 14:29:53 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -17,11 +17,11 @@ User.prototype.save = function(callback) {
 
 
 	mongodb.open(function(err, db) {
-		if( err ) {
+		if ( err ) {
 			return callback(err);
 		}
 		db.collection('users', function(err, collection) {
-			if( err ) {
+			if ( err ) {
 				mongodb.close();
 				return callback(err);
 			}
@@ -29,7 +29,7 @@ User.prototype.save = function(callback) {
 				safe: true
 			}, function(err, user) {
 				mongodb.close();
-				if( err ) {
+				if ( err ) {
 					return callback(err);
 				}
 				callback(null, user[0]);
