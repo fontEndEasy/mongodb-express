85707e75162faa3c20beb4a2633ff66f1fb08d41
^+xl.z)` <fontEndEasy@users.noreply.github.com>
2017-05-27 14:41:40 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /** 
-  * @param required Object{ req }
-  * @param required Object{ res }
-  * @param required Object{ next }
+  * @param Object{ req }
+  * @param Object{ res }
+  * @param Object{ callback: next }
   * @comment String 'GET users listing.' 
 */
 router.get('/', function(req, res, next) {
